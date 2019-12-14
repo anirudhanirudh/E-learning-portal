75b2418845395002e908780b731c41961a4f86b9
Anirudh N <43899938+anirudhanirudh@users.noreply.github.com>
2019-12-14 11:57:17 +0530

    Add files via upload

--- home.php
+++ home.php
@@ -1,58 +1,72 @@
-<!-- <h1><?php echo Escanorpage;?></h1> -->
-
-<title>home|escanor</title>
-<link href="css/bootstrap-4.3.1.css" rel="stylesheet" type="text/css">
-
-<div class="col-lg-12">  
-
-<h4>ESCANOR</h4>
-    <p> ESCANOR tutorials strives in enligtnening lives of youngsters through a free range of tutorials,We provide a wide range of vedios nad pdfs for each and every individual courses this is widely sufficient in enriching ones knowledge in a specific course.</p>
-
-</div>
-
- <div  class="col-lg-6">
-
-  
-  <p><strong>ALL IS NEED TO DO IS...</strong></p>
-  <h4>STEPS.</h4>
-  <p> Initially one has to login with their details; Once you finished registration process,select the course; Periodically take the excercises; Check for your scores;Ask for queries.</p>
-  <h4>OUR MOTO.</h4> 
-  <ol>
-    <li>Provide proficient courses for everyone.</li>  
-    <li>Easy access to knowledge</li> 
-    <li>Provide suffient basements for each courses</li> 
-    <li>Minimize the stress level in obtaining proficiency</li>  
-     
-  </ol>
-
-  <h4>OBJECTIVES.</h4> 
-   <ol>
-     
-      <li>Provide relevant trainings and seminar to faculty, staff, and student.</li>
-      <li>Produce highly competitive graduates.</li>
-     
-      
-   </ol> 
-</div>
-<div  class="col-lg-6">
-  <h4>GUIDING PRINCIPALS.</h4> 
-    <ul>
-      <li>Excellency in producing knowledge  </li>
-      <li>Responsibility</li>
-      <li> Academic Standards</li>  
-    </ul>
-<h4>Core Values</h4>
-  <ul>
-    <li>God-Centered</li>
-    <li>Excellence</li>
-    <li>Integrity</li>
-    <li>Transparency and Accountability</li>
-    <li>Dedication to Quality Service</li>  
-  </ul> 
-
-</div>
+<style type="text/css">
  
+  .secondrow > .row {
+     border: 1px solid #ddd;
+    padding: 5px 30px;*/
+    text-align: center; 
+    margin: 20px;
+    min-height: 50px;
+     border-radius: 50%;
+  }
+  .imgstretch{ 
+    padding: 10px 30px;
+  }
+  .imgstretch img {
+    width: 100%;
+    height: 100px;
+  }
 
-<script src="js/jquery-3.3.1.min.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap-4.3.1.js"></script>
+ 
+</style>
+
+<div class="container">
+  <h3>Administrator Panel:Welcome <?php echo $_SESSION['NAME'];?></h3>
+ 
+  <div class="row"> 
+    <div class="col-md-6 secondrow">
+      <div class="row">
+        <a href="<?php echo web_root; ?>admin/modules/lesson/index.php" title="Lessons"> 
+        <div class="imgstretch">
+          <img src="<?php echo web_root; ?>admin/adminMenu/images/less1.gif"> 
+         </div>
+         <label>Lessons</label>
+        </a>
+      </div>
+    </div> 
+    <div class="col-md-6 secondrow">
+      <div class="row">
+        <a href="<?php echo web_root; ?>admin/modules/exercises/index.php" title="Exercises"> 
+        <div class="imgstretch">
+          <img src="<?php echo web_root; ?>admin/adminMenu/images/exercise.jpg"> 
+         </div>
+         <label>Exercises</label>
+        </a>
+      </div>
+    </div> 
+  </div>
+    <div class="row">
+       <?php if($_SESSION['TYPE']=="Administrator"){ ?>
+    <div class="col-md-6 secondrow">
+      <div class="row">
+        <a href="<?php echo web_root; ?>admin/modules/user/index.php" title="Manage Users"> 
+        <div class="imgstretch">
+          <img src="<?php echo web_root; ?>admin/adminMenu/images/user.png"> 
+         </div>
+         <label>Manage Users</label>
+        </a>
+      </div>
+    </div>
+  <?php } ?>
+    <div class="col-md-6 secondrow">
+      <div class="row">
+        <a href="<?php echo web_root; ?>admin/modules/report/index.php" title="Reports"> 
+        <div class="imgstretch">
+          <img src="<?php echo web_root; ?>admin/adminMenu/images/report1.png"> 
+         </div>
+         <label>Reports</label>
+        </a>
+      </div>
+    </div> 
+  </div>
+  
+</div>
--- logout.php
+++ logout.php
@@ -1,20 +1,6 @@
 <?php 
-require_once 'include/initialize.php';
-// Four steps to closing a session
-// (i.e. logging out)
+require_once '../include/initialize.php';
 
-// 1. Find the session
-@session_start();
 
-// 2. Unset all the session variables
-unset( $_SESSION['StudentID'] );
-unset( $_SESSION['Fname'] );
-unset( $_SESSION['Lname'] ); 
-unset( $_SESSION['STUDUSERNAME'] );  
-unset( $_SESSION['STUDPASS'] );  
- 
- 	
-// 4. Destroy the session
-// session_destroy();
-redirect("index.php?logout=1");
+redirect(web_root."admin/login.php?logout=1");
 ?>
