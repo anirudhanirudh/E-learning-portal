2ac1a7841d273fbe0bb304769b24ee77371e7d69
Anirudh N <43899938+anirudhanirudh@users.noreply.github.com>
2019-12-14 11:46:47 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,58 @@
+<!-- <h1><?php echo Escanorpage;?></h1> -->
+
+<title>home|escanor</title>
+<link href="css/bootstrap-4.3.1.css" rel="stylesheet" type="text/css">
+
+<div class="col-lg-12">  
+
+<h4>ESCANOR</h4>
+    <p> ESCANOR tutorials strives in enligtnening lives of youngsters through a free range of tutorials,We provide a wide range of vedios nad pdfs for each and every individual courses this is widely sufficient in enriching ones knowledge in a specific course.</p>
+
+</div>
+
+ <div  class="col-lg-6">
+
+  
+  <p><strong>ALL IS NEED TO DO IS...</strong></p>
+  <h4>STEPS.</h4>
+  <p> Initially one has to login with their details; Once you finished registration process,select the course; Periodically take the excercises; Check for your scores;Ask for queries.</p>
+  <h4>OUR MOTO.</h4> 
+  <ol>
+    <li>Provide proficient courses for everyone.</li>  
+    <li>Easy access to knowledge</li> 
+    <li>Provide suffient basements for each courses</li> 
+    <li>Minimize the stress level in obtaining proficiency</li>  
+     
+  </ol>
+
+  <h4>OBJECTIVES.</h4> 
+   <ol>
+     
+      <li>Provide relevant trainings and seminar to faculty, staff, and student.</li>
+      <li>Produce highly competitive graduates.</li>
+     
+      
+   </ol> 
+</div>
+<div  class="col-lg-6">
+  <h4>GUIDING PRINCIPALS.</h4> 
+    <ul>
+      <li>Excellency in producing knowledge  </li>
+      <li>Responsibility</li>
+      <li> Academic Standards</li>  
+    </ul>
+<h4>Core Values</h4>
+  <ul>
+    <li>God-Centered</li>
+    <li>Excellence</li>
+    <li>Integrity</li>
+    <li>Transparency and Accountability</li>
+    <li>Dedication to Quality Service</li>  
+  </ul> 
+
+</div>
+ 
+
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap-4.3.1.js"></script>
